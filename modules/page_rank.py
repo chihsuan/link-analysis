ce27a847fb888f6b170e795b802beb59bcc234ad
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-21 00:01:34 +0800

    update page_rank.py

---
+++ modules/page_rank.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+import sys
+from my_class.Graph import Graph
+
+def page_rank(graph, iteration=20, damping_factor=0.85):
+    num_nodes = len(graph.nodes)
+    ranks = dict.fromkeys(graph.nodes, 1.0/num_nodes)
+    
+    min_value = 1 - damping_factor
+    for iteration in xrange(int(iteration)):
+        for node in graph.nodes:
+            rank = min_value
+            for in_node in graph.in_nodes[node]:
+                rank = damping_factor * ranks[in_node] / len(graph.out_nodes[in_node])
+            ranks[node] = rank
+    return ranks
+
--- pageRank.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python2.7
-# -*- coding: utf-8 -*-
-
