9dbeb8b04d5fb820356ee05422e651c7781ebfca
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-11-23 21:33:16 +0800

    fixed a bug

--- lp_model_graph.py
+++ lp_model_graph.py
@@ -8,18 +8,17 @@ from modules import json_io
 def get_lp_graph(grid_size):
     graph = {}
     node = 1
-    grid_size += 1
 
-    for i in range(grid_size):
-        for j in range(grid_size):
+    for i in range(grid_size+1):
+        for j in range(grid_size+1):
             graph[node] = []
             if i > 0:
-                graph[node].append(node - i * grid_size)
-            if i < (grid_size-1):
-                graph[node].append(node + i * grid_size)
+                graph[node].append(node - 1 * (grid_size+1))
+            if i < (grid_size):
+                graph[node].append(node + 1 * (grid_size+1))
             if j > 0:
                 graph[node].append(node - 1)
-            if j < (grid_size-1):
+            if j < (grid_size):
                 graph[node].append(node + 1)
             node += 1
     return graph
@@ -33,7 +32,6 @@ def rewire_edge(graph, prob):
             new_edge = random.randint(1, len(graph.keys()))
             while new_edge in out_nodes or new_edge == node:
                 new_edge = random.randint(1, len(graph.keys()))
-
             out_nodes.append(new_edge)
             out_nodes.pop(pick)
     return graph
@@ -46,7 +44,5 @@ if __name__=='__main__':
     lp_graph = get_lp_graph(4)
     rewire_graph1 = rewire_edge(lp_graph.copy(), p1)
     rewire_graph2 = rewire_edge(lp_graph.copy(), p2)
-    json_io.write_json('dataset/graph7.json', rewire_graph1)
-    json_io.write_json('dataset/graph8.json', rewire_graph2)
-
-
+    json_io.write_json('dataset/graph_7.json', rewire_graph1)
+    json_io.write_json('dataset/graph_8.json', rewire_graph2)
